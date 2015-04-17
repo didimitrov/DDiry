a988915cdce5e6e6a40b5f2ddd1621bca768974c
didimitrov <di_dimitrov_@abv.bg>
2015-04-17 10:54:51 +0300

    Added MySelf controller and Index view

---
+++ Diary/Diary.Web/Controllers/MySelfController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using AutoMapper.QueryableExtensions;
+using Diary.Data.Repository;
+using Diary.Models;
+using Diary.Web.Models;
+
+namespace Diary.Web.Controllers
+{
+    public class MySelfController : Controller
+    {
+        private readonly IRepository<MySelf> _mySelf; 
+        // GET: MySelf
+        public MySelfController(IRepository<MySelf> mySelf)
+        {
+            this._mySelf = mySelf;
+        }
+        public ActionResult Index()
+        {
+            var me = _mySelf.All()
+                .Project()
+                .To<MySelfViewModel>().ToList();
+            return View(me);
+        }
+    }
+    
+}
---
+++ Diary/Diary.Web/Models/MySelfViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Diary.Models;
+using Diary.Web.Infrastructure;
+
+namespace Diary.Web.Models
+{
+    public class MySelfViewModel:IMapFrom<MySelf>
+    {
+        public int MySelfId { get; set; }
+        [Required(ErrorMessage = "Моля,въведете име!")]
+        [Display(Name = "Име")]
+        public string Name { get; set; }
+        [Display(Name = "Рожденна дата")]
+        [DataType(DataType.DateTime)]
+        public DateTime Born { get; set; }
+        [Display(Name = "Информация")]
+        public string Info { get; set; }
+        [Display(Name = "Снимка")]
+        public string ImageUrl { get; set; }
+    }
+}
